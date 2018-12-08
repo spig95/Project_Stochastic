022354752e0a8173e1d79a21195224dfd218447c
Nicola Ischia <nicolaischia95@gmail.com>
2018-12-08 11:14:47 +0100

    Added the SEED, useful to compare methods

--- parameters.py
+++ parameters.py
@@ -10,3 +10,5 @@ u1 = 5 # "first" component of the vector of the velocity field
 u2 = 0 # "second" component of the vector of the velocity field
 R = 1
 T = 1
+
+SEED = 9012019 #fix the seed and remember the deadline
