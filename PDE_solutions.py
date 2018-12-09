456581abae5e9b3cbf4bf3f14737a3d57dc64a49
Nicola Ischia <nicolaischia95@gmail.com>
2018-12-09 19:06:30 +0100

    Notebook now rely on PDE_solutions.py

---
+++ PDE_solutions.py
@@ -0,0 +1,17 @@
+######################################################
+#               PDE SOLUTIONS                        #
+######################################################
+
+# Write here the solution of the PDE in selected points.
+# Doing so we don't need to correct them in every Notebook
+# if we recompute the solution
+
+
+# X0 = [-1.5, 1.]
+# PDEProb = 0.7132074678294174
+
+# X0 = [-1.5, 1.5]
+# PDEProb = 0.3397504218684861
+
+X0 = [-1.5, 2.]
+PDEProb = 0.0980219#2210119967
