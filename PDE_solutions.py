a48bf3730b3c43cb766755b6c1c0d0c50741971a
Nicola Ischia <nicolaischia95@gmail.com>
2018-12-10 14:21:59 +0100

    one point for every order of magnitude

--- PDE_solutions.py
+++ PDE_solutions.py
@@ -6,12 +6,20 @@
 # Doing so we don't need to correct them in every Notebook
 # if we recompute the solution
 
+X0 = [-3, 0]
+PDEProb = 0.9154860060658957
 
 # X0 = [-1.5, 1.]
-# PDEProb = 0.7132074678294174
+# PDEProb = 0.7173050315908374
 
-# X0 = [-1.5, 1.5]
-# PDEProb = 0.3397504218684861
+# X0 = [-1.5, 2.]
+# PDEProb = 0.10081408181152719
 
-X0 = [-1.5, 2.]
-PDEProb = 0.0980219#2210119967
+# X0 = [-3, 3]
+# PDEProb = 0.015134991497104936
+
+# X0 = [0, 2.5]
+# PDEProb = 0.000851609120970076
+
+# X0 = [0, 3]
+# PDEProb = 6.293701166092087e-05
