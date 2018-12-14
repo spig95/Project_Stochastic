4ab6deedbdf7c573d691da729ea2179222a15758
Nicola Ischia <nicolaischia95@gmail.com>
2018-12-14 11:36:38 +0100

    Method D

--- PDE_solutions.py
+++ PDE_solutions.py
@@ -6,14 +6,14 @@
 # Doing so we don't need to correct them in every Notebook
 # if we recompute the solution
 
-X0 = [-1.5, 1.]
-PDEProb = 0.7173050315908374
+# X0 = [-1.5, 1.]
+# PDEProb = 0.7173050315908374
 
 # X0 = [-1.5, 2.]
 # PDEProb = 0.10081408181152719
 
-# X0 = [-3, 3]
-# PDEProb = 0.015134991497104936
+X0 = [-3, 3]
+PDEProb = 0.015134991497104936
 
 # X0 = [0, 2.5]
 # PDEProb = 0.000851609120970076
